f8afe7c7d25fd6c56e38a35c1bb819bde2aecb60
Duo Francis <duofrancis11@gmil.com>
2024-01-28 22:29:01 +0000

    Setting up Authentication and Authorization

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL ='login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
